c5c29a852c5242f395dfe3f50d5c40f507e0f150
med amine haj amor <amineehajamorr@gmail.com>
2021-06-22 00:00:46 +0100

    savepoint4

--- main.js
+++ main.js
@@ -5,20 +5,31 @@ var makeCohort = function (numOfScreens,numOfBooks,numOfTables,numOfChairs,total
 	obj.tables = numOfTables
 	obj.chairs = numOfChairs
 	obj.totalWealth = totalWealth
-	obj.student = numOfStudents
 	obj.buyScreen = buyScreen
 	obj.sellScreen = sellDamagedScreen
 	obj.buyBook = buyBook
 	obj.displayChairs= displayChairs
+	obj.sellChair = sellChair
+	obj.buyChair = buyChair
+	obj.displayScreens = displayScreens
+	obj.displayBooks = displayBooks
+	obj.sellTable = sellTable
+	obj.buyTable = buyTable
+	obj.displayTables=displayTables
+	obj.depositPerMonth= depositPerMonth
 	return obj
+}
+var depositPerMonth = function (){
+this.totalWealth + 4000
+alert("money added to your vault")
 }
 var buyScreen = function (){
 	if(this.totalWealth >= 1200 ){
 	 this.screens++
 	 this.totalWealth - 1200
-	 return "you bought a new screen check your total wealth"
+	 alert ("you bought a new screen check your total wealth")
 	}
-	return "we dont have enough money"
+	alert ("we dont have enough money")
 }
 var sellDamagedScreen = function (){
 	if(this.screens > 0){
@@ -26,17 +37,69 @@ var sellDamagedScreen = function (){
 		this.totalWealth + 800
 		"you sold a used screen"
 	}
-	"we are out of screens"
+	alert("we are out of screens")
 }
 var buyBook = function (){
 	if(this.totalWealth > 20){
 		this.totalWealth - 20 
 		this.books++
-		"you bought a new book"
+		alert("you bought a new book")
 	}
-return "we dont have enough money" 
+alert( "we dont have enough money") 
+}
+var sellBook = function(){
+	if (this.book > 0){
+		this.book--
+		this.totalWealth - 10
+		alert( "you sold an old book")
+	}
+	alert("we are out of books")
 }
 
-function displayChairs (){
-	return this.chairs
+var displayChairs = function  (){
+	alert("you have " + this.chairs + " chairs")
 }
+var sellChair = function (){
+	if(this.chairs > 0){
+		this.chairs--
+		this.totalWealth + 50
+		alert('you sold an old chair')
+	}
+	alert("we are out of chairs")
+}
+
+var buyChair = function (){
+	if(this.totalWealth > 90){
+		this.chairs++
+		this.totalWealth - 90
+		alert("you bought a new chair")
+	}
+	alert("we dont have enough money")
+}
+var displayScreens = function (){
+	alert ('you have ' + this.screens + ' screens')
+}
+ var displayBooks = function () {
+ 	alert('you have ' + this.book + " books")
+ }
+
+var sellTable = function (){
+	if(this.tables > 0){
+		this.tables--
+		this.totalWealth -40
+	}
+	alert("we are out of tables")
+}
+var buyTable = function (){
+	if(totalWealth> 60){
+		this.totalWealth - 60 
+		this.tables++
+		alert("you bought a new table")
+	}
+	alert("we dont have enough money")
+}
+var displayTables = function (){
+	alert("you have " + this.tables + " tables")
+}
+
+$("")
